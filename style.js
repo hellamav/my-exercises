f88d14c268696ed2f73c70b4dbc8405369f4cd7f
hel <hel@gmail>
2023-07-13 09:12:40 +0100

    Intro to Js

---
+++ style.js
@@ -0,0 +1,50 @@
+//declare the variable age
+/*
+let age = prompt("Please enter your age");
+
+//condition that checks if age is between 18 and 65 and returns a value
+
+if( age >= 18 && age<=65) {
+    console.log("Satisfied");
+} else if (age < 18){
+    console.log("not satisfied");
+} else {
+    console.log("Please exit page");
+} */
+
+
+
+
+//use ternary-if syntax, write code that checks if age is above 50.
+
+/*
+let age = prompt("Please enter your age" )
+let result = (age >=18 && age <=65) ? "satisfied" : "Not satisfied";
+
+console.log(result);
+*/
+
+//Objects, Arrays + Json
+// creating an object using literal notation
+let darthvader = { allegiance: "empire", weapon:"lightsabre", sith:"true" };
+console.log(darthvader)
+console.log("Darth Vader's allengiance is to the", darthvader.allegiance);
+console.log("Darth Vader's allengiance is to the", darthvader.weapon);
+console.log(`Darth Vader is a sith? ${darthvader.sith}`);
+console.log(`Darth Vader is a sith? ${darthvader.sith ? "false" : "true"}`);
+
+//create an array with 2 elements
+
+let myArray = ["hell0", "everyone"];
+//find the length of the array
+console.log(myArray.length);
+//Adding elements to the array using push
+console.log(myArray.push("First element", "second element", "third element"));
+//Removing the first element from the array
+myArray.shift();
+console.log(myArray)
+//print the content of an array using the for loop
+
+for (let i in myArray) {
+    console.log(myArray);
+}
